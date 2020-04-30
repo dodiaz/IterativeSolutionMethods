bb12505ac7192ec5917a1c901846dc5cca4c713f
dominicadiaz <dad358@cornell.edu>
2020-04-30 14:32:14 -0400

    Add readme text and started the main script

--- IterativeSolvers.c
+++ IterativeSolvers.c
@@ -8,18 +8,18 @@
 
 
 //function used to print the data to a text file
-int print_current_data(int step, double** u, double** v, double** p, int NX, int NY) {
+int print_current_data(int step, double** laplace_phi, double** f, double** phi, double* error, int NX, int NY) {
     int i, j;
 
 
-    char filename1[25] = "step_";
+    char filename1[30] = "step_";
     itoa(step, filename1 + 4, 10);
-    strcat(filename1, "_u_data.txt");
+    strcat(filename1, "_laplace_phi_data.txt");
 
     FILE* fpointer1 = fopen(filename1, "w");
     for (j = 0; j < NY; j++) {
         for (i = 0; i < NX; i++) {
-            fprintf(fpointer1, "%.20lf ", u[j][i]);
+            fprintf(fpointer1, "%.20lf ", laplace_phi[j][i]);
         }
         fprintf(fpointer1, "\n");
     }
@@ -29,12 +29,12 @@ int print_current_data(int step, double** u, double** v, double** p, int NX, int
 
     char filename2[25] = "step_";
     itoa(step, filename2 + 4, 10);
-    strcat(filename2, "_v_data.txt");
+    strcat(filename2, "_f_data.txt");
 
     FILE* fpointer2 = fopen(filename2, "w");
     for (j = 0; j < NY; j++) {
         for (i = 0; i < NX; i++) {
-            fprintf(fpointer2, "%.20lf ", v[j][i]);
+            fprintf(fpointer2, "%.20lf ", f[j][i]);
         }
         fprintf(fpointer2, "\n");
     }
@@ -42,19 +42,31 @@ int print_current_data(int step, double** u, double** v, double** p, int NX, int
 
 
 
-
-    char filename3[25] = "step_";
+    char filename3[30] = "step_";
     itoa(step, filename3 + 4, 10);
-    strcat(filename3, "_p_data.txt");
+    strcat(filename3, "_phi_data.txt");
 
     FILE* fpointer3 = fopen(filename3, "w");
     for (j = 0; j < NY; j++) {
         for (i = 0; i < NX; i++) {
-            fprintf(fpointer3, "%.20lf ", p[j][i]);
+            fprintf(fpointer3, "%.20lf ", phi[j][i]);
         }
         fprintf(fpointer3, "\n");
     }
     fclose(fpointer3);
+
+
+
+    char filename4[30] = "step_";
+    itoa(step, filename4 + 4, 10);
+    strcat(filename4, "_error_data.txt");
+
+    FILE* fpointer4 = fopen(filename4, "w");
+    for (j = 0; j < step; j++) {
+        fprintf(fpointer4, "%.20lf ", error[j]);
+    }
+    fclose(fpointer4);
+
 
     return 1;
 }
@@ -64,6 +76,79 @@ int print_current_data(int step, double** u, double** v, double** p, int NX, int
 
 
 
+int main() {
+
+    /* ----------------------------------------------------------------------------------------------------------
+    Initializing variables -------------------------------------------------------------------------------- */
+    
+    char method[] = "PJ";      //possible methods "PJ", "GS", "SOR", "CG", "MG"
+    int print_now;             //just allows us to print
+
+
+
+
+    /* ----------------------------------------------------------------------------------------------------------
+    Initializing f -------------------------------------------------------------------------------- */
+
+
+
+
+
+
+    /* ----------------------------------------------------------------------------------------------------------
+    Point Jacobi method -------------------------------------------------------------------------------- */
+
+    if ( strcmp(method, "PJ") == 0 ) {
+        
+
+
+
+    }
+
+
+    /* ----------------------------------------------------------------------------------------------------------
+    Gauss Seidel method -------------------------------------------------------------------------------- */
+
+    if ( strcmp(method, "GS") == 0 ) {
+
+
+
+
+
+    }
+
+
+    /* ----------------------------------------------------------------------------------------------------------
+    Successive over-relaxation method -------------------------------------------------------------------------------- */
+
+    if ( strcmp(method, "SOR") == 0 ) {
+        
+
+
+
+    }
+
+
+    /* ----------------------------------------------------------------------------------------------------------
+    Conjugate gradient method -------------------------------------------------------------------------------- */
+
+    if ( strcmp(method, "CG") == 0 ) {
+
+
+
+
+    }
+
+
+    /* ----------------------------------------------------------------------------------------------------------
+    Multigrid method -------------------------------------------------------------------------------- */
+
+    if ( strcmp(method, "MG") == 0 ) {
+        
+
+
+
+    }
 
 
 
@@ -71,8 +156,5 @@ int print_current_data(int step, double** u, double** v, double** p, int NX, int
 
 
 
-int main()
-{
-
-
+    
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # Implementation of different iterative methods to solve poisson's equation
+
+Each of the 5 methods are implemented in one script called "IterativeSolvers":
+1. Point Jacobi
+2. Gauss-Seidel
+3. Successive over-relaxation
+4. Conjugate gradient
+5. Multigrid
+
+There is a variable called "method" that needs to be changed when looking at the different iterative methods. The possible methods are: "PJ", "GS", "SOR", "CG", or "MG".

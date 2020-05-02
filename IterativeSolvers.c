ed944da505dbdc9bd4e19b63872f4ba06948a0c5
dominicadiaz <dad358@cornell.edu>
2020-05-02 00:00:38 -0400

    finished GS and PJ

--- IterativeSolvers.c
+++ IterativeSolvers.c
@@ -8,12 +8,14 @@
 
 
 //function used to print the data to a text file
-int print_current_data(int step, double** laplace_phi, double** f, double** phi, double* error, int NX, int NY) {
+int print_current_data(int step, double** laplace_phi, double** f, double** phi, double* error, int NX, int NY, char method[]) {
     int i, j;
 
 
-    char filename1[30] = "step_";
+    char filename1[40] = "step_";
     itoa(step, filename1 + 4, 10);
+    strcat(filename1, "_");
+    strcat(filename1, method);
     strcat(filename1, "_laplace_phi_data.txt");
 
     FILE* fpointer1 = fopen(filename1, "w");
@@ -29,6 +31,8 @@ int print_current_data(int step, double** laplace_phi, double** f, double** phi,
 
     char filename2[25] = "step_";
     itoa(step, filename2 + 4, 10);
+    strcat(filename2, "_");
+    strcat(filename2, method);
     strcat(filename2, "_f_data.txt");
 
     FILE* fpointer2 = fopen(filename2, "w");
@@ -44,6 +48,8 @@ int print_current_data(int step, double** laplace_phi, double** f, double** phi,
 
     char filename3[30] = "step_";
     itoa(step, filename3 + 4, 10);
+    strcat(filename3, "_");
+    strcat(filename3, method);
     strcat(filename3, "_phi_data.txt");
 
     FILE* fpointer3 = fopen(filename3, "w");
@@ -59,6 +65,8 @@ int print_current_data(int step, double** laplace_phi, double** f, double** phi,
 
     char filename4[30] = "step_";
     itoa(step, filename4 + 4, 10);
+    strcat(filename4, "_");
+    strcat(filename4, method);
     strcat(filename4, "_error_data.txt");
 
     FILE* fpointer4 = fopen(filename4, "w");
@@ -81,40 +89,58 @@ int main() {
     /* ----------------------------------------------------------------------------------------------------------
     Initializing variables -------------------------------------------------------------------------------- */
     
-    char method[] = "PJ";      //possible methods "PJ", "GS", "SOR", "CG", "MG"
+    char method[] = "GS";      //possible methods "PJ", "GS", "SOR", "CG", "MG"
 
     int i, j;
+    int step = 0;
+    int max_num_steps = 2000;   //Increase this number if the method isn't converging
 
     int Nx = 11;
     int Ny = 11;
     double nx = Nx;
     double ny = Ny;
-    double D_x = 1 / (nx - 1);
-    double D_y = 1 / (ny - 1);
-    double x = 0;
-    double y = 0;
+    double D_x = 1 / nx;
+    double D_y = 1 / ny;
+    double x = D_x / 2;
+    double y = D_y / 2;
+
+    double f_norm;
+    double lambda = pow(D_x, -2);
+    double laplace_phi_minus_f_norm;
+    double epsilon = pow(10, -3);
+    double RHS;
+    double integral;
+
+    int print_now;
 
     /* ----------------------------------------------------------------------------------------------------------
     Initializing f -------------------------------------------------------------------------------- */
 
+    double* error = (double*)calloc(max_num_steps, sizeof(double));
+
     double** f = (double**)calloc(ny, sizeof(double*));
     double** phi = (double**)calloc(ny, sizeof(double*));
+    double** phi_new = (double**)calloc(ny, sizeof(double*));
     double** laplace_phi = (double**)calloc(ny, sizeof(double*));
 
     for (i = 0; i < nx; i++) {
         f[i] = (double*)calloc(nx, sizeof(double));
         phi[i] = (double*)calloc(nx, sizeof(double));
+        phi_new[i] = (double*)calloc(nx, sizeof(double));
         laplace_phi[i] = (double*)calloc(nx, sizeof(double));
 
     }
 
-    for (j = 0; j < ny; j++) {
-        for (i = 0; i < nx; i++) {
+    for (j = 0; j < Ny; j++) {
+        for (i = 0; i < Nx; i++) {
             f[j][i] = exp((-pow(x - 0.75, 2) - pow(y - 0.75, 2)) / pow(0.05, 2)) - exp((-pow(x - 0.25, 2) - pow(y - 0.25, 2)) / pow(0.05, 2));
-            
+
             x += D_x;
-            y += D_y;
+            
         }
+
+        y += D_y;
+        x = D_x / 2;
     }
 
     /*=============================================== SOLVERS =================================================*/
@@ -123,27 +149,307 @@ int main() {
     Point Jacobi method -------------------------------------------------------------------------------- */
 
     if ( strcmp(method, "PJ") == 0 ) {
-        for (j = 0; j < ny; j++) {
-            for (i = 0; i < nx; i++) {
-            
+
+        f_norm = 0; /* compute f_norm */
+        for (j = 0; j < Ny; j++) {
+            for (i = 0; i < Nx; i++) {
+                f_norm = f_norm + pow(f[j][i], 2);
+            }
         }
+
+        f_norm = sqrt(f_norm);
+
+
+
+
+        do {
+
+
+            for (j = 1; j < Ny - 1; j++) {
+                for (i = 1; i < Nx - 1; i++) {
+
+                    phi_new[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) / 4 - f[j][i] / (4 * lambda);
+
+                }
+
+            }
+
+            //update left boundary values
+            for (j = 1; j < Ny - 1; j++) {
+                i = 0;
+                phi_new[j][i] = (phi[j][i + 1] + phi[j - 1][i] + phi[j + 1][i]) / 3 - f[j][i] / (3 * lambda);
+
+            }
+
+            //update right boundary values
+            for (j = 1; j < Ny - 1; j++) {
+                i = Nx - 1;
+                phi_new[j][i] = (phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) / 3 - f[j][i] / (3 * lambda);
+
+            }
+
+            //update bottom boundary values 
+            for (i = 1; i < Nx - 1; i++) {
+                j = 0;
+                phi_new[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j + 1][i]) / 3 - f[j][i] / (3 * lambda);
+            }
+
+            //update top boundary values
+            for (i = 1; i < Nx - 1; i++) {
+                j = Ny - 1;
+                phi_new[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i]) / 3 - f[j][i] / (3 * lambda);
+            }
+
+            //update corner points
+            phi_new[0][0] = (phi[1][0] + phi[0][1]) / 2 - f[0][0] / (2 * lambda);
+            phi_new[0][Nx - 1] = (phi[1][Nx - 1] + phi[0][Nx - 2]) / 2 - f[0][Nx - 1] / (2 * lambda);
+            phi_new[Ny - 1][0] = (phi[Ny - 1][1] + phi[Ny - 2][0]) / 2 - f[Ny - 1][0] / (2 * lambda);
+            phi_new[Ny - 1][Nx - 1] = (phi[Ny - 1][Nx - 2] + phi[Ny - 2][Nx - 1]) / 2 - f[Ny - 1][Nx - 1] / (2 * lambda);
+
+
+
+            //update phi matrix
+            for (j = 0; j < Ny; j++) {
+                for (i = 0; i < Nx; i++) {
+                    phi[j][i] = phi_new[j][i];
+                }
+            }
+
+
+
+            //compute laplace_p matrix
+            for (j = 1; j < Ny - 1; j++) {
+                for (i = 1; i < Nx - 1; i++) {
+                    laplace_phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) * lambda - 4 * lambda * phi[j][i];
+                }
+            }
+            for (j = 1; j < Ny - 1; j++) {
+                i = 0;
+                laplace_phi[j][i] = (phi[j][i + 1] + phi[j - 1][i] + phi[j + 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            for (j = 1; j < Ny - 1; j++) {
+                i = Nx - 1;
+                laplace_phi[j][i] = (phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            for (i = 1; i < Nx - 1; i++) {
+                j = 0;
+                laplace_phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j + 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            for (i = 1; i < Nx - 1; i++) {
+                j = Ny - 1;
+                laplace_phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            laplace_phi[0][0] = (phi[1][0] + phi[0][1]) * lambda - phi[0][0] * (2 * lambda);
+            laplace_phi[0][Nx - 1] = (phi[1][Nx - 1] + phi[0][Nx - 2]) * lambda - phi[0][Nx - 1] * (2 * lambda);
+            laplace_phi[Ny - 1][0] = (phi[Ny - 1][1] + phi[Ny - 2][0]) * lambda - phi[Ny - 1][0] * (2 * lambda);
+            laplace_phi[Ny - 1][Nx - 1] = (phi[Ny - 1][Nx - 2] + phi[Ny - 2][Nx - 1]) * lambda - phi[Ny - 1][Nx - 1] * (2 * lambda);
+
+
+
+            //compute the norm
+            laplace_phi_minus_f_norm = 0;
+
+            for (j = 0; j < Ny; j++) {
+                for (i = 0; i < Nx; i++) {
+                    laplace_phi_minus_f_norm = laplace_phi_minus_f_norm + pow((laplace_phi[j][i] - f[j][i]), 2);
+                }
+            }
+
+            
+
+            laplace_phi_minus_f_norm = sqrt(laplace_phi_minus_f_norm);
+
+
+            if (f_norm == 0) {
+                RHS = epsilon;
+            }
+            else {
+                RHS = epsilon * f_norm;
+            }
+
+
+            //save the error here and break out of the loop if the max number of steps has been reached
+            error[step] = laplace_phi_minus_f_norm;
+
+            if (step == max_num_steps) {
+                break;
+            }
+
+            step += 1; 
+
+        } while (laplace_phi_minus_f_norm > RHS);
+
+
+        // Impose condition that the integral over the domain is equal to zero
+        integral = 0;
+        for (j = 0; j < Ny; j++) {
+            for (i = 0; i < Nx; i++) {
+                integral += phi[j][i] * D_x * D_y;
+            }
+        }
+
+        for (j = 0; j < Ny; j++) {
+            for (i = 0; i < Nx; i++) {
+                phi[j][i] = phi[j][i] - integral / (Nx * Ny);
+            }
+        }
+
+        print_now = print_current_data(step, laplace_phi, f, phi, error, Nx, Ny, method);
+        printf("Data was printed for Point Jacobi method");
+
+
+
+
     }
 
 
 
-    }
+    
 
 
     /* ----------------------------------------------------------------------------------------------------------
     Gauss Seidel method -------------------------------------------------------------------------------- */
 
     if ( strcmp(method, "GS") == 0 ) {
-        for (j = 0; j < ny; j++) {
-            for (i = 0; i < nx; i++) {
-            
+        
+        
+        f_norm = 0; /* compute f_norm */
+        for (j = 0; j < Ny; j++) {
+            for (i = 0; i < Nx; i++) {
+                f_norm = f_norm + pow(f[j][i], 2);
+            }
         }
-    }
 
+        f_norm = sqrt(f_norm);
+
+
+
+
+        do {
+
+
+            for (j = 1; j < Ny - 1; j++) {
+                for (i = 1; i < Nx - 1; i++) {
+
+                    phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) / 4 - f[j][i] / (4 * lambda);
+
+                }
+
+            }
+
+            //update left boundary values
+            for (j = 1; j < Ny - 1; j++) {
+                i = 0;
+                phi[j][i] = (phi[j][i + 1] + phi[j - 1][i] + phi[j + 1][i]) / 3 - f[j][i] / (3 * lambda);
+
+            }
+
+            //update right boundary values
+            for (j = 1; j < Ny - 1; j++) {
+                i = Nx - 1;
+                phi[j][i] = (phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) / 3 - f[j][i] / (3 * lambda);
+
+            }
+
+            //update bottom boundary values 
+            for (i = 1; i < Nx - 1; i++) {
+                j = 0;
+                phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j + 1][i]) / 3 - f[j][i] / (3 * lambda);
+            }
+
+            //update top boundary values
+            for (i = 1; i < Nx - 1; i++) {
+                j = Ny - 1;
+                phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i]) / 3 - f[j][i] / (3 * lambda);
+            }
+
+            //update corner points
+            phi[0][0] = (phi[1][0] + phi[0][1]) / 2 - f[0][0] / (2 * lambda);
+            phi[0][Nx - 1] = (phi[1][Nx - 1] + phi[0][Nx - 2]) / 2 - f[0][Nx - 1] / (2 * lambda);
+            phi[Ny - 1][0] = (phi[Ny - 1][1] + phi[Ny - 2][0]) / 2 - f[Ny - 1][0] / (2 * lambda);
+            phi[Ny - 1][Nx - 1] = (phi[Ny - 1][Nx - 2] + phi[Ny - 2][Nx - 1]) / 2 - f[Ny - 1][Nx - 1] / (2 * lambda);
+
+
+
+            //compute laplace_p matrix
+            for (j = 1; j < Ny - 1; j++) {
+                for (i = 1; i < Nx - 1; i++) {
+                    laplace_phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) * lambda - 4 * lambda * phi[j][i];
+                }
+            }
+            for (j = 1; j < Ny - 1; j++) {
+                i = 0;
+                laplace_phi[j][i] = (phi[j][i + 1] + phi[j - 1][i] + phi[j + 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            for (j = 1; j < Ny - 1; j++) {
+                i = Nx - 1;
+                laplace_phi[j][i] = (phi[j][i - 1] + phi[j - 1][i] + phi[j + 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            for (i = 1; i < Nx - 1; i++) {
+                j = 0;
+                laplace_phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j + 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            for (i = 1; i < Nx - 1; i++) {
+                j = Ny - 1;
+                laplace_phi[j][i] = (phi[j][i + 1] + phi[j][i - 1] + phi[j - 1][i]) * lambda - phi[j][i] * (3 * lambda);
+            }
+            laplace_phi[0][0] = (phi[1][0] + phi[0][1]) * lambda - phi[0][0] * (2 * lambda);
+            laplace_phi[0][Nx - 1] = (phi[1][Nx - 1] + phi[0][Nx - 2]) * lambda - phi[0][Nx - 1] * (2 * lambda);
+            laplace_phi[Ny - 1][0] = (phi[Ny - 1][1] + phi[Ny - 2][0]) * lambda - phi[Ny - 1][0] * (2 * lambda);
+            laplace_phi[Ny - 1][Nx - 1] = (phi[Ny - 1][Nx - 2] + phi[Ny - 2][Nx - 1]) * lambda - phi[Ny - 1][Nx - 1] * (2 * lambda);
+
+
+
+            //compute the norm
+            laplace_phi_minus_f_norm = 0;
+
+            for (j = 0; j < Ny; j++) {
+                for (i = 0; i < Nx; i++) {
+                    laplace_phi_minus_f_norm = laplace_phi_minus_f_norm + pow((laplace_phi[j][i] - f[j][i]), 2);
+                }
+            }
+
+            
+
+            laplace_phi_minus_f_norm = sqrt(laplace_phi_minus_f_norm);
+
+
+            if (f_norm == 0) {
+                RHS = epsilon;
+            }
+            else {
+                RHS = epsilon * f_norm;
+            }
+
+
+            //save the error here and break out of the loop if the max number of steps has been reached
+            error[step] = laplace_phi_minus_f_norm;
+
+            if (step == max_num_steps) {
+                break;
+            }
+
+            step += 1; 
+
+        } while (laplace_phi_minus_f_norm > RHS);
+
+
+        // Impose condition that the integral over the domain is equal to zero
+        integral = 0;
+        for (j = 0; j < Ny; j++) {
+            for (i = 0; i < Nx; i++) {
+                integral += phi[j][i] * D_x * D_y;
+            }
+        }
+
+        for (j = 0; j < Ny; j++) {
+            for (i = 0; i < Nx; i++) {
+                phi[j][i] = phi[j][i] - integral / (Nx * Ny);
+            }
+        }
+
+        print_now = print_current_data(step, laplace_phi, f, phi, error, Nx, Ny, method);
+        printf("Data was printed for Gauss-Seidel method");
 
 
 
@@ -185,8 +491,20 @@ int main() {
 
 
 
+    /* ----------------------------------------------------------------------------------------------------------
+    Freeing variables -------------------------------------------------------------------------------- */
 
+    for (i = 0; i < nx; i++) {
+        free(f[i]);
+        free(phi[i]);
+        free(phi_new[i]);
+        free(laplace_phi[i]);
+    }
 
+    free(f);
+    free(phi);
+    free(phi_new);
+    free(laplace_phi);
 
     
 }
